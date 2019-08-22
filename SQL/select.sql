47ed00362ed797efa5f04201d744d7e12070e203
greengangsta <36106756+greengangsta@users.noreply.github.com>
2019-08-22 15:45:39 +0530

    more practice with the select statement

--- SQL/select.sql
+++ SQL/select.sql
@@ -264,6 +264,96 @@ FROM
 ORDER BY first_name , last_name ASC;
 
 # Group By 
+USE employees;
+
+SELECT 
+    first_name
+FROM
+    employees
+GROUP BY first_name;
+
+SELECT DISTINCT
+    first_name
+FROM
+    employees;
+# select distinct is faster than group by
+
+SELECT 
+    first_name, COUNT(first_name)
+FROM
+    employees
+GROUP BY first_name
+ORDER BY first_name ASC;
+
+SELECT 
+    first_name, COUNT(first_name) AS names_count
+FROM
+    employees
+GROUP BY first_name;
+
+# Having clause 
+SELECT 
+    first_name, COUNT(first_name) AS names_count
+FROM
+    employees
+GROUP BY first_name
+HAVING COUNT(first_name) > 200;
+
+# Where and having used together
+SELECT 
+    first_name, COUNT(first_name) AS names_count
+FROM
+    employees
+WHERE
+    hire_date > '1999-01-01'
+GROUP BY first_name
+HAVING COUNT(first_name) < 200
+ORDER BY first_name;
+
+# Limit 
+SELECT 
+    *
+FROM
+    salaries
+ORDER BY salary DESC
+LIMIT 10;
+
+# Exercise 
+
+SELECT 
+    *, AVG(salary)
+FROM
+    salaries
+WHERE
+    salary > 120000
+GROUP BY emp_no
+ORDER BY emp_no;
+
+SELECT 
+    *, AVG(salary)
+FROM
+    salaries
+GROUP BY emp_no
+HAVING AVG(salary) > 120000;
+
+SELECT 
+    emp_no, count(emp_no)
+FROM
+    dept_emp
+WHERE
+    from_date > '2000-01-01'
+GROUP BY emp_no
+HAVING COUNT(emp_no) > 1;
+
+SELECT 
+    *
+FROM
+    dept_emp
+LIMIT 100;
+
+
+
+
 
 
 
